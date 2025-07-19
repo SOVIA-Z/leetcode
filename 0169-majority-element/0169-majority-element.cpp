ccc6534b9a4e563d9e08bd7acf1d3a45ee256f86
SOVIA-Z <sovia0922@gmail.com>
2025-07-19 21:06:26 +0530

    Time: 0 ms (100%), Space: 28.1 MB (61.4%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        //moore's voting algo  O(n)
+        int n= nums.size();
+        int frq = 0, ans =0;
+        for(int i = 0; i<n;i++){
+            if(frq == 0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){ //same element
+                frq++;
+
+            }else{ // diff ele
+                frq--;
+            }
+        }
+        return ans;
+
+
+        //sorting
+        // int n= nums.size();
+        // sort(nums.begin(),nums.end());
+        // int frq = 1, ans = nums[0];
+        // for(int i = 1; i<n;i++){
+        //     if(nums[i]== nums[i-1]){
+        //         frq++;
+        //     }
+        //     else{
+        //         frq = 1; 
+        //         ans = nums[i];
+        //     }
+
+        // }
+        // return ans;
+        
+        // brute force
+        // int n = nums.size();
+        // for(int val:nums){
+        //     int frq = 0;
+        //     for(int el:nums){
+        //         if(val== el){
+        //             frq++;
+        //         }
+        //     }
+        //     if(frq>n/2){
+        //         return val;
+        //     }
+        // }
+        // return -1;
+    }
+};
