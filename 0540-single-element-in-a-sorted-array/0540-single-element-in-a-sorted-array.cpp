0d1a061420c123811e2e33d076a684af19ecb7f8
SOVIA-Z <sovia0922@gmail.com>
2025-07-25 20:11:35 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.5%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        // int st = 0, end = A.size()-1;
-        // while(st<= end){
-        //     mid = st + (end - st)/2;
-        //     if(mid%2==0){// even 
+    int singleNonDuplicate(vector<int>& A) {
+        int n= A.size();
+        if(n==1) return A[0];
+        int st = 0, end = n-1;
+        while(st<= end){
+            int mid = st + (end - st)/2;
+
+            if(mid == 0 && A[0] != A[1]) {return A[mid];}
+            if(mid == n-1 && A[n-1] != A[n-2]) {return A[mid];}
+            if(A[mid-1]!= A[mid] && A[mid] != A[mid+1]){ return A[mid];}
+
+            if(mid%2==0){ // even
+                if(A[mid]== A[mid-1]){//left 
+                    end = mid -1;
+
+                }else{//right
+                    st = mid +1;
+                }
 
 
-        //     }else{// odd
+            }else{ // odd
+                if(A[mid]== A[mid - 1]){//right
+                    st = mid + 1;
 
-        //     }
-        // }
+                }else{//left
+                    end = mid -1;
 
-        int ans = 0;
-        for(int i= 0; i < nums.size(); i++){
-            ans = ans ^ nums[i];
+                }
+
+            }
         }
-        return ans;
+        return -1;
+
+        // int ans = 0;
+        // for(int i= 0; i < nums.size(); i++){
+        //     ans = ans ^ nums[i];
+        // }
+        // return ans;
+
     }
 };
