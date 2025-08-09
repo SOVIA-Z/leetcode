9696a0fcf331e6c6385b5e73313fd0f39b767759
SOVIA-Z <sovia0922@gmail.com>
2025-08-10 00:00:44 +0530

    Time: 39 ms (92.99%), Space: 58.1 MB (5.35%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    function inc(){
+        return ++count;
+    }
+    function dec(){
+        return --count;
+    } 
+    function reset(){
+        count = init
+        return count;
+    }  
+    return{
+        increment:inc,
+        decrement:dec,
+        reset:reset
+
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
