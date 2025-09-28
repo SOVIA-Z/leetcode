9388f11b9451a9201799b0e3cc1cc414f11cd308
SOVIA-Z <sovia0922@gmail.com>
2025-09-28 14:22:37 +0530

    Time: 0 ms (100%), Space: 76.8 MB (81.62%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        while(head != NULL){
+            ListNode* t = head -> next;
+            head ->next= prev;
+            prev = head;
+            head = t;
+        }
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        ListNode* head = nullptr;
+        ListNode* tail = nullptr;
+        int carry = 0;
+
+        while(l1 || l2 || carry){
+            int sum = carry;
+            if(l1){
+                sum += l1-> val;
+                l1 = l1-> next;
+            }
+            if(l2){
+                sum += l2-> val;
+                l2 = l2->next;
+            }
+            carry = sum/10;
+            int temp = sum %10;
+
+            ListNode* newNode = new ListNode(temp);
+
+            if(head == nullptr){
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail -> next = newNode;
+                tail = newNode;
+            }
+        }
+        return reverse(head);
+    }
+};
