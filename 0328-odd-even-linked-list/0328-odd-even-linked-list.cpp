c4bf3dd11f297aa86d73ccd8ab4ce2bf2b09bdb0
SOVIA-Z <sovia0922@gmail.com>
2025-10-03 19:06:23 +0530

    Time: 0 ms (100%), Space: 15.6 MB (63.14%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -18,12 +18,12 @@ public:
         ListNode *even_start = head->next;
         
         while(odd->next && even->next){
-            odd->next = even->next; //Connect all odds
-            even->next = odd->next->next;  //Connect all evens
+            odd->next = even->next; 
+            even->next = odd->next->next;  
             odd = odd->next;
             even = even->next;
         }
-        odd->next = even_start;   //Place the first even node after the last odd node.
+        odd->next = even_start;   
         return head; 
         
     }
