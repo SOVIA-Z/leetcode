fbeece53ac23238c24ab9348cfd70029ccb6e7f3
SOVIA-Z <sovia0922@gmail.com>
2026-03-13 23:03:22 +0530

    Time: 43 ms (84.74%), Space: 29.1 MB (71.93%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,26 +5,26 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
+            if (i > 0 && nums[i] == nums[i-1]) { // same ele 
                 continue;
             }
             
             int j = i + 1;
             int k = nums.size() - 1;
 
-            while (j < k) {
+            while (j < k) {// when j cross k the we have iter for all ele 
                 int total = nums[i] + nums[j] + nums[k];
 
-                if (total > 0) {
+                if (total > 0) {// then we need to desc the sum 
                     k--;
-                } else if (total < 0) {
+                } else if (total < 0) { //we need to inc the sum 
                     j++;
                 } else {
                     res.push_back({nums[i], nums[j], nums[k]});
                     j++;
 
                     while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+                        j++;// checking same element 
                     }
                 }
             }
