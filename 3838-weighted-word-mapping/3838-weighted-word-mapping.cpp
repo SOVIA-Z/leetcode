b92abe02ca572d5a2cfa44cf5143b58a882f80f7
SOVIA-Z <sovia0922@gmail.com>
2026-06-14 01:02:20 +0530

    Time: 0 ms (100%), Space: 43.5 MB (77.6%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+
+        for(auto &word: words){
+            int sum = 0;
+
+            for(char c : word){
+                sum+= weights[c - 'a'];
+            }
+
+            int rem = sum% 26;
+
+            ans += ('z'- rem);
+            
+        }
+        return ans;
+        
+    }
+};
